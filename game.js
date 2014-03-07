e5b8295fcf3cf6308ef6613f8c257d0f86b768b8
Eric Fawcett <elfawcett@gmail.com>
2014-03-07 16:14:42 -0500

    added some tilesets and maps, and started refactoring things into classes

--- game.js
+++ game.js
@@ -14,6 +14,12 @@ var
   // Backgrounds
   defaultBG
 
+  // Tilemaps
+, map
+
+  // Layers
+, layer
+
   // Sprites
 , player
 , platforms
@@ -24,6 +30,8 @@ var
 , cursors
 ;
 
+
+
 /*===============================
 =            preload            =
 ===============================*/
@@ -32,7 +40,14 @@ function preload() {
   game.load.image('defaultBG', 'assets/defaultBG.png');
 
   /*==========  Platforms  ==========*/
-  game.load.image('defaultPlatform', 'assets/defaultPlatform.png');
+  // game.load.image('defaultPlatform', 'assets/defaultPlatform.png');
+  // Tilemaps
+  game.load.tilemap('level'      , 'assets/tilemaps/test.json', null, Phaser.Tilemap.TILED_JSON );
+  game.load.image('tilemap-test' , 'assets/tilemaps/test.png')
+
+  // Player
+  player = new Player( game );
+  player.preload();
   
 }
 
@@ -40,17 +55,38 @@ function preload() {
 =            create            =
 ==============================*/
 function create() {
+  /*==========  Base game options  ==========*/
+  game.stage.backgroundColor = '#c3c3c3';
+  game.physics.gravity.y = 250;
+  game.physics.setBoundsToWorld();
+
+
   /*==========  Background  ==========*/
-  defaultBG = game.add.tileSprite( 0, 0, 640, 480, 'defaultBG');
+  // defaultBG = game.add.tileSprite( 0, 0, 640, 480, 'defaultBG');
+  // defaultBG.fixedToCamera = true;
+
+  // Tilemap
+  map = game.add.tilemap('level');
+  map.addTilesetImage('tilemap-test');
+  map.setCollisionByExclusion([]);
+
+  layer = map.createLayer('Tile Layer 1');
+  layer.resizeWorld();
 
   /*==========  Initial platforms and ground  ==========*/
-  platforms = game.add.group();
+  // platforms = game.add.group();
 
-  var ground = platforms.create( 0, game.world.height - 64, 'defaultPlatform');
+  // var ground = platforms.create( 0, game.world.height - 16, 'defaultPlatform');
+  // ground.body.immovable = true;
+  // ground.fixedToCamera = true;
+  // var ground = game.add.tileSprite( 0, ( game.world.height - 16 ), 640, 16, 'defaultPlatform');
 
   for ( var i = 0; i < 12; i++ ) {
 
   }
+
+  // Player
+  player.create();
 }
 
 /*==============================
@@ -59,6 +95,7 @@ function create() {
 function update() {
   /*==========  Background  ==========*/
   // defaultBG.tilePosition.y += 0.5;
+  game.physics.collide( player, layer );
   
 
 }
@@ -69,3 +106,56 @@ function update() {
 function render() {
 
 }
+
+
+/*======================================================
+=            Classes - move these out later            =
+======================================================*/
+
+/*==========  Player  ==========*/
+var Player = function( game ) {
+  // Set some conveniences
+  this.game   = game;
+  this.sprite = null;
+  // may need to pass in cursors for input if using input on things other than player which would add something like this.cursors = cursors
+};
+
+Player.prototype = {
+  preload : function() {
+    this.game.load.spritesheet('player', 'assets/player/player_32x32.png', 32, 32 ); // Change this to 'spritesheet.png' eventually, containing all 32x32 sprites for player
+  }
+
+, create  : function() {
+    this.sprite = game.add.sprite( 32, 32, 'player');
+
+    // Set physics of player
+    this.sprite.body.minVelocity.y = 5;
+    this.sprite.body.collideWorldBounds = true;
+    // this.sprite.body.
+
+    // Set animations of player
+  }
+
+, update  : function() {
+    // Stop motion on every frame
+    // this.sprite.body.velocity.x = 0;
+
+    // Handle user input
+  }
+, render  : function() {}
+};
+
+/*==========  Level  ==========*/
+var Level = function( game ) {
+  this.game = game;
+};
+
+Level.prototype = {
+  preload : function() {}
+, create  : function() {}
+, update  : function() {}
+, render  : function() {}
+};
+
+
+
